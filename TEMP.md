8ce5e20e67a482d84f3f884c4d720c4956a5c0fd
Michael Epelboim <sdrmike@outlook.com>
2021-04-06 10:34:06 +0800

    added wechat login controller

--- TEMP.md
+++
@@ -1,1 +0,0 @@
-this is a temp text file
---
+++ app/controllers/login_controller.rb
@@ -0,0 +1,21 @@
+class LoginController < ApplicationController
+  URL = "https://api.weixin.qq.com/sns/jscode2session".freeze
+  def wechat_user
+    wechat_params = {
+      appId: ENV["WECHAT_APP_ID"],
+      secret: ENV["WECHAT_APP_SECRET"],
+      js_code: params[:code],
+      grant_type: "authorization_code"
+    }
+
+    @wechat_response ||= RestClient.get(URL, params: wechat_params)
+    @wechat_user ||= JSON.parse(@wechat_response.body)
+  end
+
+  def login
+    @user = User.find_or_create_by(open_id: wechat_user.fetch("openid"))
+    render json: {
+      userId: @user.id
+    }
+  end
+end
