ba0b15755461afa45671d44a7fc504c8f4a8a0b1
Jiana Zhu <jiana.zhu@gmail.com>
2021-04-06 14:23:29 +0800

    create all the models

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,48 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 2021_04_06_061416) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
+  create_table "bookings", force: :cascade do |t|
+    t.string "status"
+    t.bigint "user_id", null: false
+    t.bigint "slot_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["slot_id"], name: "index_bookings_on_slot_id"
+    t.index ["user_id"], name: "index_bookings_on_user_id"
+  end
+
+  create_table "slots", force: :cascade do |t|
+    t.date "date"
+    t.time "time"
+    t.string "restaurant_name"
+    t.string "restaurant_photo"
+    t.string "restaurant_address"
+    t.float "latitude"
+    t.float "longtitude"
+    t.bigint "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_slots_on_user_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.string "name"
+    t.string "gender"
+    t.integer "age"
+    t.string "profession"
+    t.string "favorie_cuisine"
+    t.text "self_introduction"
+    t.string "photo"
+  end
+
+  add_foreign_key "bookings", "slots"
+  add_foreign_key "bookings", "users"
+  add_foreign_key "slots", "users"
 end
