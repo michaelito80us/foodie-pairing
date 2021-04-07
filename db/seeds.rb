27b21d85f0f1c53b35016e2dbda730ad3fe16c1a
Michael Epelboim <sdrmike@outlook.com>
2021-04-07 11:55:04 +0800

    completed the index and show with jbuilder

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,4 +4,50 @@
 # Examples:
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
+
+5.times do
+  n = 10
+  namem = User.new(
+    name: Faker::FunnyName.name,
+    gender: 'male',
+    age: rand(25..50),
+    profession: Faker::Job.title,
+    favorie_cuisine: Faker::Restaurant.type,
+    self_introduction: Faker::GreekPhilosophers.quote,
+    photo: "https://randomuser.me/api/portraits/men/#{n}.jpg"
+  )
+  puts "user male: #{namem}"
+  namem.save!
+  n+=5
+end
+
+
+5.times do
+  n = 10
+  namef = User.new(
+    name: Faker::FunnyName.name,
+    gender: 'female',
+    age: rand(25..50),
+    profession: Faker::Job.title,
+    favorie_cuisine: Faker::Restaurant.type,
+    self_introduction: Faker::GreekPhilosophers.quote,
+    photo: "https://randomuser.me/api/portraits/women/#{n}.jpg"
+  )
+  puts "user female: #{namef}"
+  namef.save!
+  n+=5
+end
 #   Character.create(name: 'Luke', movie: movies.first)
+15.times do
+  s = Slot.new(
+    latitude: Faker::Address.latitude,
+    longtitude: Faker::Address.longitude,
+    restaurant_address: Faker::Address.street_address,
+    restaurant_name: Faker::Restaurant.name,
+    date: Faker::Date.forward(days: 23),
+    time: '21:00',
+    user_id: rand(1..10)
+  )
+  puts "slot: #{s}"
+  s.save!
+end
