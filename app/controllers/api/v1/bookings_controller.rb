23adbf306ee7bb1a0c39c74f2c4d6d28b42865c9
Wing Yee Kam <evawykam@gmail.com>
2021-04-08 18:40:18 +0800

    slot confirmation backend update

--- app/controllers/api/v1/bookings_controller.rb
+++ app/controllers/api/v1/bookings_controller.rb
@@ -1,7 +1,7 @@
 class Api::V1::BookingsController < Api::V1::BaseController
   # before_action :set_user, only: %i[create]
   # before_action :set_slot, only: %i[create]
-  before_action :set_booking, only: %i[show]
+  before_action :set_booking, only: %i[show update]
 
   # def index
   #   # @booking = Booking.joins(:slots).where({ bookings: { status: 'confirmed' } })
@@ -23,10 +23,18 @@ class Api::V1::BookingsController < Api::V1::BaseController
     # @bookings = Booking.where(user_id: @user.id)
   end
 
+  def update
+    if @booking.update(booking_params)
+      render json: { msg: "Updated" }
+    else
+      render_error(@bookings)
+    end
+  end
+
   private
 
   def booking_params
-    params.require(:booking).permit(:user_id, :slot_id)
+    params.require(:booking).permit(:user_id, :slot_id, :status)
   end
 
   def set_slot
@@ -40,5 +48,5 @@ class Api::V1::BookingsController < Api::V1::BaseController
   def set_booking
     @booking = Booking.find(params[:id])
   end
-  
+
 end
